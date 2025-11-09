accc0eebe820f617327fc1a2458ee309e1906040
AnanyaGuntur8 <ananyasaiguntur@gmail.com>
2025-11-08 23:34:15 -0500

    Commit Skeleton LOGIN AND SIGNUP

--- smart-city-dashboard/electron/main.cjs
+++ smart-city-dashboard/electron/main.cjs
@@ -14,7 +14,7 @@ const createWindow = () => {
 
   if (isDev) {
     win.loadURL('http://localhost:5173');
-    win.webContents.openDevTools();
+    // win.webContents.openDevTools();
   } else {
     win.loadFile(path.join(__dirname, '../dist/index.html'));
   }
---
+++ smart-city-dashboard/src/Login.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom"; // <-- IMPORTANT
+import "./Login.css";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Login will be implemented once backend is ready!");
+    console.log({ email, password });
+  };
+
+  return (
+    <div className="login-container">
+      {/* Left: Form */}
+      <div className="login-left">
+        <div className="login-card">
+          <h2>Login</h2>
+          <form onSubmit={handleSubmit}>
+            <input 
+              type="email" 
+              placeholder="Email" 
+              value={email} 
+              onChange={(e) => setEmail(e.target.value)} 
+              required 
+            />
+            <input 
+              type="password" 
+              placeholder="Password" 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)} 
+              required 
+            />
+            <button type="submit">Login</button>
+          </form>
+
+          <p>
+            Don't have an account?{" "}
+            <Link to="/signup">Sign Up</Link> {/* <-- Real routing link */}
+          </p>
+        </div>
+      </div>
+
+      {/* Right: Glass City SVG */}
+      <div className="login-right">
+        <svg className="city-svg-login" viewBox="0 0 600 400" xmlns="http://www.w3.org/2000/svg">
+          <g className="city-lines-login">
+            <path d="M0,400 L0,300 L40,300 L40,400 Z" />
+            <path d="M60,400 L60,250 L100,250 L100,400 Z" />
+            <path d="M120,400 L120,200 L160,200 L160,400 Z" />
+            <path d="M180,400 L180,320 L220,320 L220,400 Z" />
+            <path d="M240,400 L240,180 L280,180 L280,400 Z" />
+            <path d="M300,400 L300,240 L340,240 L340,400 Z" />
+          </g>
+        </svg>
+      </div>
+    </div>
+  );
+}
